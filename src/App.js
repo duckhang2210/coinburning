1530b197f6b9c23e5cb4f449ab8d3d0982067c47
Duc Khang Nguyen <duckhang2210@gmail.com>
2021-10-25 15:24:30 -0500

    footer template

--- src/App.js
+++ src/App.js
@@ -32,11 +32,18 @@ const App = () => {
                         </Switch>
                     </div>
                 </Layout>
-            </div>
-            <div className="footer">
-
-            </div>
-            
+                <div className="footer">
+                    <Typography.Title level={5} style={{color: 'white', textAlign: 'center'}}>
+                        Coin Burning <br />
+                        All Rights Reserved
+                    </Typography.Title>
+                    <Space>
+                        <Link to='/'>Home</Link>
+                        <Link to='/exchanges'>Exchanges</Link>
+                        <Link to='/news'>News</Link>
+                    </Space>
+                </div>
+            </div>    
         </div>
     )
 }
